b05c1b4f490586d37743dea96a0e6acfd56883f5
Daniel Guerra <wordgames01@gmail.com>
2021-01-17 13:22:34 +0100

    initial commit. migrate teacher project and algorithm to my unity project. Implemented basic UI elemets.

---
+++ Assets/Scripts/Bandit.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bandit : MonoBehaviour
+{
+    public bool init;
+    public int totalActions;
+    public int[] count;
+    public float[] score;
+
+    public int numActions;
+    public RPSAction lastAction;
+    public int lastStrategy;
+
+    float initialRegret = 10f;
+    public float[] regret;
+    public float[] chance;
+    public RPSAction lastOpponentAction;
+    public RPSAction[] lastActionRM;
+
+    public Sprite sprites;
+    SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        InitRegretMatching();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Se va a ejecutar el algoritmo (por parte de la IA) para jugar, se cambia además el sprite por la acción escogida.
+    public RPSAction Jugar()
+    {
+        RPSAction resultado;
+
+        resultado = GetNextActionRM();
+        // TODO: Colocar el token en la posición de la acción tomada
+
+        return resultado;
+    }
+
+    // Se le da al oponente información de la elección de acción escogida
+    public void TellOpponentAction(RPSAction action)
+    {
+        TellOpponentActionRM(action);
+    }
+
+    // Se inicializa el algoritmo, comprobando si ya se habia llamado previamente
+    public void InitRegretMatching()
+    {
+        if (init)
+            return;
+        numActions = System.Enum.GetNames(typeof(RPSAction)).Length;
+        regret = new float[numActions];
+        chance = new float[numActions];
+        lastActionRM = new RPSAction[numActions];
+        int i;
+        for (i = 0; i < numActions; i++)
+        {
+            regret[i] = initialRegret;
+            chance[i] = 0f;
+        }
+        init = true;
+    }
+
+    // Se usa el algoritmo del Regret Matching para decidir la siguiente acción a escoger
+    public RPSAction GetNextActionRM()
+    {
+        float sum = 0f;
+        float prob = 0f;
+        int i;
+
+        InitRegretMatching();
+
+        for (i = 0; i < numActions; i++)
+        {
+            if (regret[i] > 0f)
+                sum += regret[i];
+        }
+        if (sum <= 0f)
+        {
+            lastAction = (RPSAction)Random.Range(0, numActions);
+            return lastAction;
+        }
+        for (i = 0; i < numActions; i++)
+        {
+            chance[i] = 0f;
+            if (regret[i] > 0f)
+                chance[i] = regret[i];
+            if (i > 0)
+                chance[i] += chance[i - 1];
+        }
+        if (chance[numActions - 1] > 0)
+        {
+            prob = Random.Range(0, chance[numActions - 1]);
+            for (i = 0; i < numActions; i++)
+            {
+                if (prob < chance[i])
+                {
+                    lastAction = (RPSAction)i;
+                    return lastAction;
+                }
+            }
+        }
+        return (RPSAction)(numActions - 1);
+    }
+
+    // Se le dice al oponente la acción escogida con el Regret Matching
+    public void TellOpponentActionRM(RPSAction action)
+    {
+        int i;
+        for (i = 0; i < numActions; i++)
+        {
+            regret[i] += GetUtility((RPSAction)i, action);
+            regret[i] -= GetUtility(lastAction, action);
+        }
+    }
+
+    public RPSAction GetActionForStrategy(RPSAction strategy)
+    {
+        RPSAction action;
+        switch (strategy)
+        {
+            default:
+            case RPSAction.Red:
+                action = RPSAction.Red;
+                break;
+            case RPSAction.Black:
+                action = RPSAction.Black;
+                break;
+        }
+        return action;
+    }
+
+    // Actualizo la utilidad (el remordimiento) al final de la ronda en función de los resultados
+    public float GetUtility(RPSAction myAction, RPSAction opponents)
+    {
+        float utility = 0f;
+        if (opponents == RPSAction.Red)
+        {
+            if (myAction == RPSAction.Black)
+                utility = -1f;
+            else if (myAction == RPSAction.Red)
+                utility = 1f;
+        }
+        else if (opponents == RPSAction.Black)
+        {
+            if (myAction == RPSAction.Red)
+                utility = 1f;
+            else if (myAction == RPSAction.Black)
+                utility = -1f;
+        }
+        return utility;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Bandit ia;
+    public SpriteRenderer player;
+    public Sprite sprites;
+
+    public int scoreIA, scorePlayer;
+    public Text textScoreIA, textScorePlayer;
+
+    private void Start()
+    {
+        for(int i = 0; i < 10; ++i)
+        {
+            IAPlay();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textScoreIA.text = scoreIA.ToString();
+        textScorePlayer.text = scorePlayer.ToString();
+    }
+
+    public void IAPlay()
+    {
+
+        RPSAction iaAction = ia.Jugar();
+        ia.TellOpponentAction((RPSAction)iaAction);
+
+        Debug.LogWarning(iaAction);
+    }
+
+    public void PlayerPlay(int action)
+    {
+
+    }
+}
